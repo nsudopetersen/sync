88631c02c890b0a6f67fdfca0efd57c03a746f1d
Nicolay Petersen <nicolay.petersen@mailbox.org>
2024-10-22 15:05:15 +0200

    init

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFERSIZE 128
+
+typedef struct tinfo {
+  unsigned int wait_ms;
+} tinfo;
+
+static int buffer[BUFFERSIZE];
+static unsigned int consume_at;
+static unsigned int produce_at;
+
+void *generate_coordinates(void *);
+
+int main(int argc, char **argv) {
+  pthread_t tid;
+  pthread_attr_t taddr;
+  pthread_attr_init(&taddr);
+  tinfo tinfo;
+  tinfo.wait_ms = 500;
+
+  if (pthread_create(&tid, &taddr, generate_coordinates, (void *)&tinfo)) {
+  }
+
+  return 0;
+}
+
+void *generate_coordinates(void *tinfo) {
+  // (tinfo) *tinfo
+  for (int i = 0; i < 5; i++)
+    printf("i am alive\n");
+  pthread_exit(0);
+}
+
+void crashme(unsigned int e) {
+  printf("crashme %u\n", e);
+  exit(e);
+}
