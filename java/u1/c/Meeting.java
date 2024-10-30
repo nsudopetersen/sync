85b7fdfbfbfe00d654d354ac88213f49a75763de
Nicolay Petersen <nicolay.petersen@mailbox.org>
2024-10-30 22:16:13 +0100

    ...

--- java/u1/c/Meeting.java
+++ java/u1/c/Meeting.java
@@ -1,31 +1,7 @@
 /* */
 
 public class Meeting {
-
-    private static final int LINE_COUNT = 3;
-
-    public static void main(String[] args) {
-        String separator;
-        Scanner s;
-        String[] input = new String[3];
-
-        separator = args[0];
-        s = new Scanner(System.in);
-
-        for(int i = 0; i < OutputDelimited.LINE_COUNT; i++)
-        {
-            input[i] = s.nextLine().toLowerCase();
-        }
-        
-        for(int i = 0; i < OutputDelimited.LINE_COUNT; i++)
-        {
-            System.out.print(input[i]);
-            if(i + 1 < OutputDelimited.LINE_COUNT) {
-                System.out.print(separator);
-            }
-        }
-        System.out.println("");
-
-        s.close();
+    
+    public static void func(String[] args) {
     }
 }
