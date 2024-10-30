d4e1c60d4dc5332297f9f7b4c1b0dfd7b3ef61cb
Nicolay Petersen <nicolay.petersen@mailbox.org>
2024-10-30 21:49:57 +0100

    ...

--- java/test/SimpleProgram.java
+++
@@ -1,7 +0,0 @@
-/* */
-
-public class SimpleProgram {
-    public static void main(String[] args) {
-        System.out.println("ran");
-    }
-}
--- java/u1/SimpleProgram.java
+++ java/u1/SimpleProgram.java
@@ -1,7 +1,26 @@
 /* */
 
-public class SimpleProgram {
+public class OutputDelimited {
+
+    private static final int LINE_COUNT = 3;
+
     public static void main(String[] args) {
-        System.out.println("ran");
+        private String separator;
+        private Scanner s;
+        private String input[];
+
+        separator = args[0];
+        s = new Scanner(System.in);
+
+        for(int i = 0; i < LINE_COUNT; i++)
+        {
+            input[i] = s.nextline().toLowerCase() + separator;
+        }
+        
+        for(int i = 0; i < LINE_COUNT; i++)
+        {
+            System.out.print(input[i]);
+        }
+        System.out.println();
     }
 }
