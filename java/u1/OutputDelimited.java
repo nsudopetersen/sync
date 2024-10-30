67d194b27ec168cbe3fc05687564999fe2d9b0c9
Nicolay Petersen <nicolay.petersen@mailbox.org>
2024-10-30 21:53:36 +0100

    ...

--- java/u1/OutputDelimited.java
+++ java/u1/OutputDelimited.java
@@ -5,22 +5,23 @@ public class OutputDelimited {
     private static final int LINE_COUNT = 3;
 
     public static void main(String[] args) {
-        private String separator;
-        private Scanner s;
-        private String input[];
+        String separator;
+        Scanner s;
+        String input[];
+        int i;
 
         separator = args[0];
         s = new Scanner(System.in);
 
-        for(int i = 0; i < LINE_COUNT; i++)
+        for(; i < 3; i++)
         {
             input[i] = s.nextline().toLowerCase() + separator;
         }
         
-        for(int i = 0; i < LINE_COUNT; i++)
+        for(; i < 3; i++)
         {
             System.out.print(input[i]);
         }
-        System.out.println();
+        System.out.println("");
     }
 }
